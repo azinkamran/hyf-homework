78648f6f3d1fb142b8bc057d65e27aa838adf625
azinkamran <48134836+azinkamran@users.noreply.github.com>
2019-07-17 12:27:40 +0200

    added

---
+++ javascript/javascript3/week2/promise.js
@@ -0,0 +1,17 @@
+let p;
+function call(resolveAfter) {
+  p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("I am called asynchronously", resolveAfter);
+      reject("error");
+    }, resolveAfter);
+  });
+}
+
+call(3000);
+p.then(console.log);
+p.catch(console.log);
+
+call(6000);
+p.then(console.log);
+p.catch(console.log);
