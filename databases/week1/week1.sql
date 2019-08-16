69abb2ef7027091db5dbd5faa518a217cb74e80c
azinkamran <48134836+azinkamran@users.noreply.github.com>
2019-08-16 11:46:55 +0200

    added

---
+++ databases/week1/week1.sql
@@ -0,0 +1,21 @@
+use hyf19;
+-- 1- Find out how many tasks are in the task table
+-- --select count(id) from task;
+-- 2- Find out how many tasks in the task table do not have a valid due date
+-- --select count(due_date)from task where due_date is not null
+-- 3- Find all the tasks that are marked as done
+-- -- select title from task where status_id = 3
+-- 4- Find all the tasks that are not marked as done
+-- --select title, status_id from task where status_id != 3
+-- 5- Get all the tasks, sorted with the most recently created first
+-- -- select * from task order by created desc
+-- 6- Get the single most recently created task
+-- --select * from task order by created desc limit 1
+-- 7-Get the title and due date of all tasks where the title or description contains database
+-- -- select title,due_date from task where title like '%database%' or description like '%database%'
+-- 8- Get the title and status (as text) of all tasks
+-- select title,status.name from task inner join status on task.status_id = status.id 
+-- 9- Get the name of each status, along with a count of how many tasks have that status
+-- -- select status.name, count(task.status_id) from task inner join status on status.id = task.status_id group by status.name
+-- 10- Get the names of all statuses, sorted by the status with most tasks first
+select status.name, count(task.status_id)as count from task inner join status on status.id = task.status_id group by status.name order by count desc
