436f357f49b4a5a79954da314855188584477c0d
azinkamran <48134836+azinkamran@users.noreply.github.com>
2019-09-23 09:28:36 +0200

    updated

--- nodejs/week3/claculator/src/index.js
+++ nodejs/week3/claculator/src/index.js
@@ -1,44 +1,58 @@
 const express = require('express');
 const app = express();
 
-
+//http://localhost:3002/calculator/multiply?firstParam=5&secondParam=2
 app.get('/calculator/multiply', (req, res) => {
     const numbers = [];
     for (const key in req.query) {
         const number = Number(req.query[key]);
         numbers.push(number);
     };
+    console.log(numbers);
     const multiplication = numbers.reduce((total, amount) => total * amount);
 
     res.send(multiplication.toString());
 })
-
-
-app.get('/calculator', (req, res) => {
-    const key = req.query.key;
+//http://localhost:3002/calculator/division?firstParam=5&secondParam=2
+app.get('/calculator/division', (req, res) => {
     const numbers = [];
-    let response;
+    for (const key in req.query) {
+        const number = Number(req.query[key]);
+        numbers.push(number);
+    };
+    console.log(numbers);
+    const division = numbers.reduce((total, amount) => total / amount);
 
-    for (const param in req.query) {
-        const number = Number(req.query[param]);
-        // Push only numbers to the array
-        if (!isNaN(number)) {
-            numbers.push(number);
-        }  
-    }
-
-    if (key === 'multiply') {
-        response = (numbers.reduce((total, amount) => total * amount)).toString();
-    } else if (key === 'division') {
-        response = (numbers.reduce((total, amount) => total / amount)).toString();
-    }else if (key === 'plus') {
-      response = (numbers.reduce((total, amount) => total + amount)).toString();
-  }else if (key === 'minus') {
-    response = (numbers.reduce((total, amount) => total - amount)).toString();
-}
-
-    res.send(response);
+    res.send(division.toString());
 })
+
+//http://localhost:3002/calculator/add?firstParam=5&secondParam=2
+app.get('/calculator/add', (req, res) => {
+    const numbers = [];
+    for (const key in req.query) {
+        const number = Number(req.query[key]);
+        numbers.push(number);
+    };
+    console.log(numbers);
+    const add = numbers.reduce((total, amount) => total + amount);
+
+    res.send(add.toString());
+})
+
+//http://localhost:3002/calculator/subtract?firstParam=5&secondParam=2
+app.get('/calculator/subtract', (req, res) => {
+    const numbers = [];
+    for (const key in req.query) {
+        const number = Number(req.query[key]);
+        numbers.push(number);
+    };
+    console.log(numbers);
+    const subtract = numbers.reduce((total, amount) => total - amount);
+
+    res.send(subtract.toString());
+})
+
+
 
 app.listen(3002, () => {
     console.log('Server is listening at 3002');
